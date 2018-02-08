106b6030ebe59d3f491cb505c8c9179a0da8e819
Zach Cossman <Zachcos@gmail.com>
2018-02-08 17:16:21 -0500

    Add static layout for CityView component

--- app/components/CityView.jsx
+++ app/components/CityView.jsx
@@ -4,7 +4,78 @@ export class CityView extends React.Component {
   render() {
     return (
         <div className="col-sm">
-          <h1>This is CityView!</h1>
+          <h1 className="text-center mt-5 mb-5">Portland</h1>
+          <div className="col-sm-8 offset-sm-2">
+            <div className="input-group input-group-md mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="inputGroup-sizing-md">HHD</span>
+                <span className="input-group-text" id="inputGroup-sizing-md">$123</span>
+              </div>
+              <input type="text" className="form-control" />
+              <div className="input-group-append">
+                <button class="btn btn-outline-secondary" type="button">Max Amount</button>
+              </div>
+            </div>
+
+            <div className="input-group input-group-md mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="inputGroup-sizing-md">SSD</span>
+                <span className="input-group-text" id="inputGroup-sizing-md">$123</span>
+              </div>
+              <input type="text" className="form-control" />
+              <div className="input-group-append">
+                <button class="btn btn-outline-secondary" type="button">Max Amount</button>
+              </div>
+            </div>
+
+            <div className="input-group input-group-md mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="inputGroup-sizing-md">GPUs</span>
+                <span className="input-group-text" id="inputGroup-sizing-md">$123</span>
+              </div>
+              <input type="text" className="form-control" />
+              <div className="input-group-append">
+                <button class="btn btn-outline-secondary" type="button">Max Amount</button>
+              </div>
+            </div>
+
+            <div className="input-group input-group-md mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="inputGroup-sizing-md">CPUs</span>
+                <span className="input-group-text" id="inputGroup-sizing-md">$123</span>
+              </div>
+              <input type="text" className="form-control" />
+              <div className="input-group-append">
+                <button class="btn btn-outline-secondary" type="button">Max Amount</button>
+              </div>
+            </div>
+
+            <div className="input-group input-group-md mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="inputGroup-sizing-md">RAM</span>
+                <span className="input-group-text" id="inputGroup-sizing-md">$123</span>
+              </div>
+              <input type="text" className="form-control" />
+              <div className="input-group-append">
+                <button class="btn btn-outline-secondary" type="button">Max Amount</button>
+              </div>
+            </div>
+
+            <div className="input-group input-group-md mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="inputGroup-sizing-md">Software</span>
+                <span className="input-group-text" id="inputGroup-sizing-md">$123</span>
+              </div>
+              <input type="text" className="form-control" />
+              <div className="input-group-append">
+                <button class="btn btn-outline-secondary" type="button">Max Amount</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm text-center mt-5">
+            <button type="button" className="btn btn-success mr-2">Buy Parts</button>
+            <button type="button" className="btn btn-danger">Sell Parts</button>
+          </div>
         </div>
       
     )
