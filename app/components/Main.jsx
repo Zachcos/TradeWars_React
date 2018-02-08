c786a28c680abff29db032a7d28523b20ad6469e
Zach Cossman <Zachcos@gmail.com>
2018-02-08 16:05:38 -0500

    Add static layout to Main.jsx

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 export class Main extends React.Component {
   render () {
-    return <h1>This was added from Main.jsx</h1>
+    return (
+      <div>
+        <nav id="main-nav" className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a href="#" className="navbar-brand">TradeWars</a>
+        </nav>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm">City View</div>
+            <div className="col-sm">Player View</div>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
