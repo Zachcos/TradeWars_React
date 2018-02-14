bc543421def8f33491e189ea26390e781db29b18
Zach Cossman <Zachcos@gmail.com>
2018-02-14 02:18:16 -0500

    Display props in render Products

--- app/components/Product.jsx
+++ app/components/Product.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 
-export class Product extends React.Component {
-  render() {
-    return (
-      <div className="input-group input-group-md mb-3">
-        <div className="input-group-prepend">
-          <span className="input-group-text" id="inputGroup-sizing-md">TEST</span>
-          <span className="input-group-text" id="inputGroup-sizing-md">$123</span>
-        </div>
-        <input type="text" className="form-control" />
-        <div className="input-group-append">
-          <button class="btn btn-outline-secondary" type="button">Max Amount</button>
-        </div>
+const Product = ({ product }) => {
+  return (
+    <div className="input-group input-group-md mb-3">
+      <div className="input-group-prepend">
+        <span className="input-group-text" id="inputGroup-sizing-md">{product.name}</span>
+        <span className="input-group-text" id="inputGroup-sizing-md">${product.price}</span>
       </div>
-    )
-  }
+      <input type="text" className="form-control" />
+      <div className="input-group-append">
+        <button class="btn btn-outline-secondary" type="button">Max Amount</button>
+      </div>
+    </div>
+  )
 }
 
 export default Product;
