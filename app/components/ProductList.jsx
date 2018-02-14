20249c47d0219b111d296285e9e0af8fcbc06ca6
Zach Cossman <Zachcos@gmail.com>
2018-02-14 02:15:02 -0500

    Add map through products in state and render Product component for each

--- app/components/ProductList.jsx
+++ app/components/ProductList.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import Product from 'Product';
 
-export const ProductList = ({ products }) => {
-  return <Product />
+const ProductList = ({ products }) => {
+  const renderList = () => {
+    return products.map(product => {
+      return <Product product={product} />
+    })
+  }  
+  
+  return (
+    <div className="product-list">
+      {renderList()}
+    </div>
+  )
 }
 
 export default ProductList;
