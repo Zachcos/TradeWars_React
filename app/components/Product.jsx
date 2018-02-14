60e9abc9cf93e90ad7d64aca0406e72c55639c37
Zach Cossman <Zachcos@gmail.com>
2018-02-14 01:51:20 -0500

    Move product information in Product and remove static product data

--- app/components/Product.jsx
+++ app/components/Product.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 
 export class Product extends React.Component {
   render() {
-    return <h1>This is a Product component</h1>
+    return (
+      <div className="input-group input-group-md mb-3">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="inputGroup-sizing-md">TEST</span>
+          <span className="input-group-text" id="inputGroup-sizing-md">$123</span>
+        </div>
+        <input type="text" className="form-control" />
+        <div className="input-group-append">
+          <button class="btn btn-outline-secondary" type="button">Max Amount</button>
+        </div>
+      </div>
+    )
   }
 }
 
