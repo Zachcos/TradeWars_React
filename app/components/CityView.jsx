2f3b3772a4aa91502092f59df8776cbe6c62eec8
Zach Cossman <Zachcos@gmail.com>
2018-02-15 14:33:42 -0500

    CityView refactored to functional component

--- app/components/CityView.jsx
+++ app/components/CityView.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import ProductList from 'ProductList';
 
-export class CityView extends React.Component {
-  render() {
-    return (
-        <div className="col-sm">
-          <h1 className="text-center mt-5 mb-5">Portland</h1>
-          <div className="col-sm-8 offset-sm-2">
-            <ProductList products={this.props.products}/>
-          </div>
-        </div> 
-    )
-  }
+const CityView = ({ currentPlayer, products }) => {
+  return (
+    <div className="col-sm">
+      <h1 className="text-center mt-5 mb-5">Portland</h1>
+      <div className="col-sm-8 offset-sm-2">
+        <ProductList products={products}/>
+      </div>
+    </div> 
+)
 }
 
 export default CityView;
