87ea57c41afd4de2c67d0ef3d0227c9edc443654
Zach Cossman <Zachcos@gmail.com>
2018-02-15 16:34:47 -0500

    Refactor Product component to stateful

--- app/components/Product.jsx
+++ app/components/Product.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 
-const Product = ({ product, handleChange, handleTransaction }) => {
-  return (
-    <div className="input-group input-group-md mb-3">
-      <div className="input-group-prepend">
-        <span className="input-group-text" id="inputGroup-sizing-md">{product.name}</span>
-        <span className="input-group-text" id="inputGroup-sizing-md">${product.price}</span>
-      </div>
-      <input type="text" className="form-control" onChange={handleChange} {...product}/>
-      <div className="input-group-append">
-        <button className="btn btn-outline-secondary" type="button">Max.</button>
-        <button className="btn btn-success" type="button" onClick={handleTransaction}>Buy</button>
-        <button className="btn btn-danger" type="button" onClick={handleTransaction}>Sell</button>
+export class Product extends React.Component {  
+  render() {
+    const { product, handleChange, handleTransaction } = this.props;
 
+    return (
+      <div className="input-group input-group-md mb-3">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="inputGroup-sizing-md">{product.name}</span>
+          <span className="input-group-text" id="inputGroup-sizing-md">${product.price}</span>
+        </div>
+        <input type="text" className="form-control" onChange={handleChange} {...product}/>
+        <div className="input-group-append">
+          <button className="btn btn-outline-secondary" type="button">Max.</button>
+          <button className="btn btn-success" type="button" onClick={handleTransaction}>Buy</button>
+          <button className="btn btn-danger" type="button" onClick={handleTransaction}>Sell</button>
+  
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Product;
