2ee657cc2a47d96fe5cf3026704cd66571371e05
Zach Cossman <Zachcos@gmail.com>
2018-02-17 12:25:08 -0500

    Add quantityAvailable property to products

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -18,15 +18,18 @@ export class Main extends React.Component {
       products: [
         {
           name: 'CPU',
-          price: 100
+          price: 100,
+          quantityAvailable: 4
         },
         {
           name: 'GPU',
-          price: 200
+          price: 200,
+          quantityAvailable: 4
         },
         {
           name: 'HHD',
-          price: 300
+          price: 300,
+          quantityAvailable: 4
         },
       ]
     }
