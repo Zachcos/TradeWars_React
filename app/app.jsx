59b7d04a63e662b7cbe570f29214b26c3d4d5aba
Zach Cossman <Zachcos@gmail.com>
2018-03-01 11:06:38 -0500

    Nest router inside Provider

--- app/app.jsx
+++ app/app.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import Main from 'Main';
 
+const store = configureStore();
+
 ReactDOM.render(
-  <BrowserRouter>
-    <Main />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Main />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('app')
 )
