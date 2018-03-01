009cc2731467f356a75b8dd395d47dd444ee67a5
Zach Cossman <Zachcos@gmail.com>
2018-03-01 10:59:38 -0500

    Nest main component in app.jsx index BroswerRouter component

--- app/app.jsx
+++ app/app.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import Main from 'Main';
 
 ReactDOM.render(
-  <Main />,
+  <BrowserRouter>
+    <Main />
+  </BrowserRouter>,
   document.getElementById('app')
 )
