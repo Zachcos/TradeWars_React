d8abd9d5121cbab07f524c5fb2d9b4c23a10683f
Zach Cossman <Zachcos@gmail.com>
2018-03-01 13:02:08 -0500

    Add property to products

--- app/reducers/initialState.jsx
+++ app/reducers/initialState.jsx
@@ -3,17 +3,20 @@ export default {
     {
       name: 'CPU',
       price: 100,
-      quantityAvailable: 4
+      quantityAvailable: 4,
+      id: 1,
     },
     {
       name: 'GPU',
       price: 200,
-      quantityAvailable: 4
+      quantityAvailable: 4,
+      id: 2,
     },
     {
       name: 'HHD',
       price: 300,
-      quantityAvailable: 4
+      quantityAvailable: 4,
+      id: 3,
     }
   ]
 }
