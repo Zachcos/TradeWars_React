6db0d49b284214908f457ee4910d21dbaae727f9
Zach Cossman <Zachcos@gmail.com>
2018-03-01 11:18:47 -0500

    Add intialState and code to allow Redux Dev Extension to work

---
+++ app/reducers/initialState.jsx
@@ -0,0 +1,19 @@
+export default {
+  products: [
+    {
+      name: 'CPU',
+      price: 100,
+      quantityAvailable: 4
+    },
+    {
+      name: 'GPU',
+      price: 200,
+      quantityAvailable: 4
+    },
+    {
+      name: 'HHD',
+      price: 300,
+      quantityAvailable: 4
+    },
+  ]
+}
