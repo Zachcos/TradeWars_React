ed110545931671eefda8461e35f237833d7999a2
Zach Cossman <Zachcos@gmail.com>
2018-03-04 15:41:36 -0500

    Quantities entered in the quantityField are successfully subtracted from the proper product in the redux store

--- app/components/ProductListItem.jsx
+++ app/components/ProductListItem.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
+import * as actions from '../actions/actions';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 export class ProductListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.startTransation = this.startTransation.bind(this);
+  }
+  
+  startTransation(event) {
+    event.preventDefault();
+    console.log("id: " + this.props.product.id)
+    console.log("quant: " + this.quantField.value)
+    
+    const update = {
+      id: this.props.product.id,
+      quantity: this.quantField.value
+    }
+
+    this.props.actions.testAction(update)
+  }
+  
   render() {
     const { product, handleTransaction } = this.props;
 
@@ -11,10 +33,10 @@ export class ProductListItem extends React.Component {
         <span className="input-group-text" id="inputGroup-sizing-md">${product.price}</span>
         <span className="input-group-text" id="inputGroup-sizing-md">{product.quantityAvailable} avail.</span>
       </div>
-      <input type="text" className="form-control" />
+      <input type="text" className="form-control" ref={(quantField) => { this.quantField = quantField }} />
       <div className="input-group-append">
         {/* <button className="btn btn-outline-secondary" type="button">Max.</button> */}
-        <button className="btn btn-success" type="button" onClick={handleTransaction}>Buy</button>
+        <button className="btn btn-success" type="button" onClick={this.startTransation}>Buy</button>
         <button className="btn btn-danger" type="button" onClick={handleTransaction}>Sell</button>
 
       </div>
@@ -23,4 +45,10 @@ export class ProductListItem extends React.Component {
   }
 }
 
-export default ProductListItem;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  }
+}
+
+export default connect((state => state), mapDispatchToProps)(ProductListItem);
