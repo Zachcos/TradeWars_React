caab30a04ffda44d976f841a86e4d115097f2404
Zach Cossman <Zachcos@gmail.com>
2018-03-10 09:02:52 -0500

    Remove unneeded handleTransaction function from Main.jsx

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -14,52 +14,12 @@ export class Main extends React.Component {
       currentError: ''
     }
 
-    this.handleTransaction = this.handleTransaction.bind(this);
     this.callError = this.callError.bind(this);
   }
 
   callError(error) {
     this.setState({ currentError: error })
     $("#errorModal").modal()
-  }
-
-  handleTransaction(action) {
-    const currentPlayer = this.state.currentPlayer;
-
-    const payload = {
-      name: action.product.name,
-      price: action.product.price,
-      quantity: action.quantity,
-      totalPrice: action.totalPrice
-    }
-
-    var index = currentPlayer.stash.findIndex(function(item) {
-      return item.name == payload.name
-    })
-
-    if (action.type == "Buy") {
-      if (action.totalPrice > currentPlayer.funds) {
-        var error = "You can't afford that"
-        this.callError(error)
-      } else if (action.quantity > action.product.quantityAvailable) {
-        var error = "There aren't enough to buy"
-        this.callError(error)
-      } else if (index != -1) {
-        console.log("we found an exisiting product")
-      } else {
-        console.log("we're going to buy stuff")
-      }
-    } else if (action.type == "Sell") {
-       if (currentPlayer.stash[index] == undefined) {
-        var error = "You don't have any of that product to sell!"
-        this.callError(error)
-      } else if (payload.quantity > currentPlayer.stash[index].quantity) {
-        var error = "You don't have that many to sell"
-        this.callError(error)
-      } else {
-        console.log("we're going to sell")
-      }
-    }
   }
 
   render () {
