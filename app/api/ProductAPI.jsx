c631d46ad381c892b5956658b04642c0d34e2770
Zach Cossman <Zachcos@gmail.com>
2018-03-19 03:08:09 -0400

    Refactor API call to ensure data is not being mutated

--- app/api/ProductAPI.jsx
+++ app/api/ProductAPI.jsx
@@ -1,9 +1,12 @@
 module.exports = {
   // Randomize price data before loading app
   randomizeData: function ( products ) {
-     return products.map(prod => {
-      prod.price = Math.floor(Math.random() * prod.max_price)
-      prod.quantityAvailable = Math.floor(Math.random() * 50)
-    })
+    products.map(prod => {
+      return {
+        ...prod,
+        price: Math.floor(Math.random() * prod.max_price),
+        quantityAvailable: Math.floor(Math.random() * 50)
+      }
+   })
   }
 }
