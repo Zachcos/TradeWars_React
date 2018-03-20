73b2932819372ca9a9da3fc0d2bb5d412de78a18
Zach Cossman <Zachcos@gmail.com>
2018-03-20 15:10:18 -0400

    Add Modal.jsx for easy resuse

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import CityView from 'CityView';
 import PlayerView from 'PlayerView';
 import ProductAPI from '../api/ProductAPI';
+import Modal from 'Modal';
 
 import { updateProducts } from '../actions/actions';
 
@@ -28,24 +29,7 @@ export class Main extends React.Component {
         <a href="#" className="navbar-brand">TradeWars</a>
         </nav>
         <div className="container-fluid">
-        <div className="modal fade" id="errorModal" tabIndex="-1" role="dialog" aria-labelledby="errorModalLabel" aria-hidden="true">
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="errorModalLabel">Error</h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div className="modal-body">
-                <p id="modal-msg"></p>
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-              </div>
-            </div>
-          </div>
-        </div>
+          <Modal />
           <div className="row">
             <CityView products={this.props.products} />
             <PlayerView currentPlayer={this.props.currentPlayer} />
