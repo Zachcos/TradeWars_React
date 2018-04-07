301e349745367a8cf787a5507bacf7b4a456ed43
Zach Cossman <Zachcos@gmail.com>
2018-04-07 00:43:38 -0400

    Refactor citySelector from stateless component to class

--- app/components/CitySelector.jsx
+++ app/components/CitySelector.jsx
@@ -1,28 +1,44 @@
 import React from 'react';
 
-const CitySelector = ({ beginTravel }) => {
-  return (
-    <div className="col-sm-4 offset-sm-4">
-      <div className="input-group">
-        <div className="input-group-prepend">
-          <label htmlFor="travelCitySelect" className="input-group-text">Location:</label>
+  class CitySelector extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        selectedCity: ''
+      }
+
+      this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+      this.setState({selectedCity: event.target.value})
+    }
+
+    render() {
+      return (
+        <div className="col-sm-4 offset-sm-4">
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <label htmlFor="travelCitySelect" className="input-group-text">Location:</label>
+            </div>
+            <select name="travelCitySelect" id="travelCitySelect" className="custom-select" onChange={this.handleChange}>
+              <option defaultValue>Choose...</option>
+              <option value="Portland">Portland</option>
+              <option value="Seattle">Seattle</option>
+              <option value="Denver">Denver</option>
+              <option value="Austin">Austin</option>
+              <option value="San Francisco">San Francisco</option>
+              <option value="Silicon Valley">Silicon Valley</option>
+              <option value="New York">New York</option>
+              <option value="Raleigh">Raleigh</option>
+            </select>
+            <div className="input-group-append">
+              <button className="btn btn-primary" onClick={this.props.beginTravel}>Travel</button>
+            </div>
+          </div>
         </div>
-        <select name="travelCitySelect" id="travelCitySelect" className="custom-select">
-          <option value="Portland">Portland</option>
-          <option value="Seattle">Seattle</option>
-          <option value="Denver">Denver</option>
-          <option value="Austin">Austin</option>
-          <option value="San Francisco">San Francisco</option>
-          <option value="Silicon Valley">Silicon Valley</option>
-          <option value="New York">New York</option>
-          <option value="Raleigh">Raleigh</option>
-        </select>
-        <div className="input-group-append">
-          <button className="btn btn-primary" onClick={beginTravel}>Travel</button>
-        </div>
-      </div>
-    </div>
-  )
-}
+      )
+    }
+  }
 
 export default CitySelector;
