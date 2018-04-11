db8e2ad73457f3f50205216619161d64dc7b9cc6
Zach Cossman <Zachcos@gmail.com>
2018-04-11 19:17:43 -0400

    Track quantField.value in state to verify value !== 0

--- app/components/ProductListItem.jsx
+++ app/components/ProductListItem.jsx
@@ -6,9 +6,14 @@ export class ProductListItem extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      quantFieldValue: 0
+    }
+
     this.startTransaction = this.startTransaction.bind(this);
     this.callError = this.callError.bind(this);
     this.validateChars = this.validateChars.bind(this);
+    this.quantState = this.quantState.bind(this);
   }
 
   callError(msg) {
@@ -24,8 +29,13 @@ export class ProductListItem extends React.Component {
     }
   }
 
+  quantState(e) {
+    this.setState({quantFieldValue: this.quantField.value * 1})
+  }
+
   startTransaction(event) {
     event.preventDefault();
+    this.setState({quantFieldValue: 0})
     const transactionType = event.target.innerHTML;
     const { product, currentPlayer } = this.props;
 
@@ -37,7 +47,9 @@ export class ProductListItem extends React.Component {
     }
 
     if (transactionType === "Buy") {
-      if (this.quantField.value > product.quantityAvailable) {
+      if (this.state.quantFieldValue === 0) {
+        return;
+      } else if (this.quantField.value > product.quantityAvailable) {
         this.callError("There aren't enough of that product to buy")
       } else if (transactionData.totalPrice > currentPlayer.funds) {
         this.callError("You don't have enough money for that!")
@@ -48,7 +60,9 @@ export class ProductListItem extends React.Component {
       this.quantField.value = '';
     } else if (transactionType === "Sell") {
       const existingProduct = currentPlayer.stash.find(prod => prod.id === transactionData.id);
-      if (existingProduct === undefined) {
+      if (this.state.quantFieldValue === 0) {
+        return;
+      } else if (existingProduct === undefined) {
         this.callError("You don't have any of that product!")
       } else if (this.quantField.value > existingProduct.quantity) {
         this.callError("You don't have that many to sell!")
@@ -70,7 +84,7 @@ export class ProductListItem extends React.Component {
         <span className="input-group-text" id="inputGroup-sizing-md">${product.price}</span>
         <span className="input-group-text" id="inputGroup-sizing-md">{product.quantityAvailable} avail.</span>
       </div>
-      <input type="text" className="form-control" onKeyPress={this.validateChars} ref={(quantField) => { this.quantField = quantField }} />
+      <input type="text" className="form-control" onKeyPress={this.validateChars} onChange={this.quantState} ref={(quantField) => { this.quantField = quantField }} />
       <div className="input-group-append">
         <button className="btn btn-success" type="button" onClick={this.startTransaction}>Buy</button>
         <button className="btn btn-danger" type="button" onClick={this.startTransaction}>Sell</button>
