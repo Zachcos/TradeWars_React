5d246f22264985ad443fcacf145f56601bf51f24
Zach Cossman <Zachcos@gmail.com>
2018-07-12 09:10:57 -0400

    Refactor LoanShark component and connect to redux

--- app/components/LoanShark.jsx
+++ app/components/LoanShark.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
+import {connect} from 'react-redux';
+export class LoanShark extends React.Component {
+  render () {
+    const currentPlayer = this.props.currentPlayer;
 
-const LoanShark = ({currentPlayer}) => {
-  return (
-    <div className="col-sm-4 offset-sm-1 mt-5">
-      <h2 style={{marginBottom: 25}}>Loan Shark</h2>
-      <div className="input-group">
-        <div className="input-group-prepend">
-          <span className="input-group-text">${currentPlayer.debt} in debt</span>
-        </div>
-        <input type="text" placeholder="Enter amount" className="form-control" />
-        <div className="input-group-append">
-          <button className="btn btn-success" type="button">Pay back</button>
-          <button className="btn btn-danger" type="button">Take out</button>
+    return (
+      <div className="col-sm-4 offset-sm-1 mt-5">
+        <h2 style={{marginBottom: 25}}>Loan Shark</h2>
+        <div className="input-group">
+          <div className="input-group-prepend">
+            <span className="input-group-text">${currentPlayer.debt} in debt</span>
+          </div>
+          <input type="text" placeholder="Enter amount" className="form-control" />
+          <div className="input-group-append">
+            <button className="btn btn-success" type="button">Pay back</button>
+            <button className="btn btn-danger" type="button">Take out</button>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
-export default LoanShark;
+export default connect((state => state))(LoanShark);
