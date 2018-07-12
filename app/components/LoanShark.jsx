404a5306ac20f895260862dbc23498495433c1c5
Zach Cossman <Zachcos@gmail.com>
2018-07-12 00:31:55 -0400

    Use currentPlayer prop to display current amount of debt

--- app/components/LoanShark.jsx
+++ app/components/LoanShark.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
-const LoanShark = () => {
-  return <h1>This is the loan shark</h1>
+const LoanShark = ({currentPlayer}) => {
+  return (
+    <div className="col-sm-8 offset-sm-2">
+      <h2>Loan Shark</h2>
+      <br />
+      <p>You are ${currentPlayer.debt} in debt.</p>
+    </div>
+  )
 }
 export default LoanShark;
